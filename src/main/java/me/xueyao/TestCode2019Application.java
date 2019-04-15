3fe77baa1f7729bfe8d75903d1da4f2ecfc8576f
xueyao <xueyaome@dingtalk.com>
2019-04-15 09:58:21 +0800

    'init'

---
+++ src/main/java/me/xueyao/TestCode2019Application.java
@@ -0,0 +1,13 @@
+package me.xueyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCode2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCode2019Application.class, args);
+    }
+
+}
