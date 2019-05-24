6219808d0ce087135617b15928ef7b9e30c479bd
xueyao <xueyaome@dingtalk.com>
2019-05-24 17:59:37 +0800

    Apache-POI导出学习

--- apache-poi/src/main/java/me/xueyao/controller/TestController.java
+++ apache-poi/src/main/java/me/xueyao/controller/TestController.java
@@ -4,8 +4,13 @@ import com.alibaba.fastjson.JSONObject;
 import me.xueyao.common.BaseResponse;
 import me.xueyao.common.BaseStatus;
 import me.xueyao.entity.ExcelFile;
+import me.xueyao.entity.UserInfo;
 import me.xueyao.util.ExcelHandleUtil;
 import me.xueyao.util.UploadFileUtil;
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -15,16 +20,24 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author: Simon.Xue
@@ -59,5 +72,58 @@ public class TestController {
         Map<Integer, Map<Integer, Object>> map = ExcelHandleUtil.excelToMap(filePath, fileType, true);
         logger.info("map = {}", JSONObject.toJSONString(map));
         return response;
+    }
+
+    @GetMapping("/export")
+    public BaseResponse exportExcel() throws ClassNotFoundException {
+
+        HSSFWorkbook sheets = new HSSFWorkbook();
+        HSSFSheet xlsSheet = sheets.createSheet("成绩表");
+        List<UserInfo> userInfos = new ArrayList<>();
+        UserInfo userInfo = new UserInfo();
+
+        for (int i = 0; i < 10; i++) {
+            userInfo.setUsername("小明" + i);
+            userInfo.setEmail("xueyao.me" + i + "@gmail.com");
+            userInfo.setAge(i);
+            userInfo.setSex(1);
+            userInfo.setMobile("");
+            userInfos.add(userInfo);
+        }
+
+        HSSFRow row = xlsSheet.createRow(0);
+
+        HSSFCell cell = row.createCell(0);
+        cell.setCellValue("用户信息表");
+        HSSFRow row1 = xlsSheet.createRow(1);
+        HSSFCell cell1 = row1.createCell(0);
+        cell1.setCellValue("用户名");
+        row1.createCell(1).setCellValue("手机号");
+        row1.createCell(2).setCellValue("年龄");
+        row1.createCell(3).setCellValue("性别");
+        row1.createCell(4).setCellValue("邮箱");
+        int lastRowNum = xlsSheet.getLastRowNum();
+        Class<?> aClass = Class.forName("me.xueyao.entity.UserInfo");
+        int length = aClass.getDeclaredFields().length;
+
+        for (int i = lastRowNum+1; i < length + lastRowNum; i++) {
+            int j = 0;
+            HSSFRow row2 = xlsSheet.createRow(i);
+            row2.createCell(0).setCellValue(userInfos.get(j).getUsername());
+            row2.createCell(1).setCellValue(userInfos.get(j).getMobile());
+            row2.createCell(2).setCellValue(userInfos.get(j).getAge());
+            row2.createCell(3).setCellValue(userInfos.get(j).getSex());
+            row2.createCell(4).setCellValue(userInfos.get(j).getEmail());
+            j++;
+        }
+
+        File file = new File("a.xls");
+        try {
+            sheets.write(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
---
+++ apache-poi/src/main/java/me/xueyao/entity/UserInfo.java
@@ -0,0 +1,54 @@
+package me.xueyao.entity;
+
+/**
+ * @author: Simon.Xue
+ * @date: 2019/5/24 16:13
+ */
+public class UserInfo {
+
+    private String username;
+    private String mobile;
+    private Integer age;
+    private Integer sex;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
