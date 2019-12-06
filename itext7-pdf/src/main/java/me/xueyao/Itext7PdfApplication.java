36df982796f845c09a479149d0a8e45e1d53c84b
simonxue <xueyao.me@gmail.com>
2019-12-06 18:07:01 +0800

    导出PDF-示例

---
+++ itext7-pdf/src/main/java/me/xueyao/Itext7PdfApplication.java
@@ -0,0 +1,13 @@
+package me.xueyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itext7PdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itext7PdfApplication.class, args);
+    }
+
+}
